ecb29f5452cc95e5729a01d52e0b9536b2bd8405
simp7 <JHParkOfficial@protonmail.com>
2023-12-26 17:12:08 +0900

    Add interceptor for client.

--- productinfo/client/main.go
+++ productinfo/client/main.go
@@ -15,8 +15,43 @@ const (
 	address = "localhost:50051"
 )
 
+func orderUnaryClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	log.Println("Method: " + method)
+	err := invoker(ctx, method, req, reply, cc, opts...)
+
+	log.Println(reply)
+	return err
+}
+
+func clientStreamInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	log.Println("====== [Client Interceptor] ", method)
+	s, err := streamer(ctx, desc, cc, method, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return newWrappedStream(s), nil
+}
+
+type wrappedStream struct {
+	grpc.ClientStream
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Printf("===== [Client Stream Interceptor] "+"Receive a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Printf("===== [Client Stream Interceptor] "+"Send a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.SendMsg(m)
+}
+
+func newWrappedStream(s grpc.ClientStream) grpc.ClientStream {
+	return &wrappedStream{s}
+}
+
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithUnaryInterceptor(orderUnaryClientInterceptor), grpc.WithStreamInterceptor(clientStreamInterceptor))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
